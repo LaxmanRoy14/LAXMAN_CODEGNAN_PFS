c43935e83e23bad0b00f1286d89392a6aa77ee85
LaxmanRoy14 <laxmanroy85002@gmail.com>
2026-03-19 23:38:39 +0530

    Added Day 3 - Salary Breakdown Calculator

---
+++ Day3/day3.py
@@ -0,0 +1,26 @@
+#SALARY BREAKDOWN CALCULATOR
+print("Employee Details")
+print("=" * 40)
+employee_name = input("Enter employee name : ")
+base_salary = float(input("Enter base salary : "))
+bonus_percentage = float(input("Enter the bonus percentage : "))
+tax_percentage = float(input("Enter the tax deduction percentage : "))
+bonus = (bonus_percentage / 100) * base_salary
+gross_salary = base_salary + bonus
+tax = (tax_percentage / 100) * gross_salary
+net_salary = gross_salary - tax
+print("=" * 40)
+
+print("=" * 15, "Salary Report", "=" * 15)
+print("Employee Name : ", employee_name)
+print()
+print("Base Salary : ", base_salary)
+print("Bonus (%) : ", bonus_percentage)
+print("Tax (%) : \n", tax_percentage)
+print()
+print("Bonus Amount : ", bonus)
+print("Gross Salary : ", gross_salary)
+print("Tax Deduction : ", tax)
+print()
+print("Net Salary : ", net_salary)
+print("=" * 40)
